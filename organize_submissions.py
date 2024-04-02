4fe100c8b41008c3c04cf41562984a710cb6d2fd
Thomas Kidd <thomas.kidd@okstate.edu>
2024-04-01 21:17:22 -0500

    testing

--- organize_submissions.py
+++ organize_submissions.py
@@ -8,6 +8,7 @@ def unzip_file(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
         print(f"Extracted {zip_path} to {extract_to}")
+        print("testig")
 
 def organize_submissions(submissions_dir, student_names):
     """Organize and unzip submissions in the given directory."""
